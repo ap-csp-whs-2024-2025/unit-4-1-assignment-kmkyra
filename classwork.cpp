bfd619791bdb350ecad2387c574bd099aaa975d8
kmkyra <kennedykyra69@gmail.com>
2024-12-12 21:01:45 +0000

    variable

--- classwork.cpp
+++ classwork.cpp
@@ -3,7 +3,13 @@
 
 int main()
 {
-  // write code here
-  
+  int age = 10;
+  double price = 19.99;
+  bool isRaining = false;
+  std::cout << age << " " << price << " " << isRaining << std::endl;
+  int hello = 190000000;
+  std::cout << hello << std::endl; 
+  int grade_level = 11;
+  std::cout << "I am an " << grade_level << "th grader." << std:: endl;
   return 0;
 }
